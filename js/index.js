885fe241bcff93f75e23b985430b7ba2c5dd6587
trapcode.io <trapcode.io@gmail.com>
2020-03-18 12:06:48 +0400

    Exclude dist from git

--- js/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Abolish_1 = __importDefault(require("./src/Abolish"));
-exports.Abolish = Abolish_1.default;
-const Functions_1 = require("./src/Functions");
-exports.Rule = Functions_1.Rule;
