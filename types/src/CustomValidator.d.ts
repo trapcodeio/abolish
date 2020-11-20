50144e4878f0995db0db85f1cc46084d1e83dd11
trapcodeio <trapcode.io@gmail.com>
2020-11-20 15:10:00 +0100

    Final Fix

--- types/src/CustomValidator.d.ts
+++
@@ -1,6 +0,0 @@
-import { AbolishValidatorFunction } from "./Types";
-declare class CustomValidator {
-    fn: AbolishValidatorFunction;
-    constructor(fn: AbolishValidatorFunction);
-}
-export = CustomValidator;
