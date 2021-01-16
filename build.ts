fb896dae465a99850007ec9b19f48b0eaeb681d1
trapcodeio <trapcode.io@gmail.com>
2021-01-16 03:37:32 +0100

    Refactored

--- build.ts
+++ build.ts
@@ -6,10 +6,15 @@ import fs = require("fs");
 
 async function main() {
     const PackageDotJson = require('./package.json');
+
+    // Modify Package.json
     PackageDotJson.main = "index.js";
     PackageDotJson.types = "index.d.ts";
 
+    // Copy Package.json
     fs.writeFileSync(`${__dirname}/js/package.json`, JSON.stringify(PackageDotJson, null, 2));
+    // Copy readme.md
+    fs.copyFileSync(`${__dirname}/readme.md`, `${__dirname}/js/readme.md`);
 }
 
 main().then(() => process.exit())
