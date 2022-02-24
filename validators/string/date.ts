34cdf0d1ef77f8e1e846117891522e716535fcdf
TrapCode <48396844+trapcodeio@users.noreply.github.com>
2022-02-24 11:16:38 +0100

    Add cast support for date

--- validators/string/date.ts
+++ validators/string/date.ts
@@ -2,13 +2,20 @@ import type { AbolishValidator } from "../../src/Types";
 
 export = <AbolishValidator>{
     name: "date",
-    validator: (v, o, { modifier }) => {
+    validator: (v, o: "cast" | { cast: true }, { modifier }) => {
         if (v instanceof Date) {
             return true;
         } else if (typeof v === "string") {
             const date = new Date(v);
             const isDate = !isNaN(date.getTime());
-            if (isDate && o?.cast) modifier.setThis(date);
+
+            if (
+                isDate &&
+                ((typeof o === "string" && o === "cast") || (typeof o === "object" && o?.cast))
+            ) {
+                modifier.setThis(date);
+            }
+
             return isDate;
         } else {
             return false;
