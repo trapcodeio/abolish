2949f8d632378b6fad2ede57f67e05f4e46aeec1
trapcodeio <trapcode.io@gmail.com>
2022-07-07 10:10:28 +0100

    use validator.ts

---
+++ src/validator.ts
@@ -0,0 +1,32 @@
+/**
+ * This is not an automatically generated file.
+ */
+import { $errorRule, $errorsRule, AbolishInlineValidator, AbolishRule } from "./types";
+
+export declare module AvailableValidators {
+    export interface Options {
+        $name: string;
+        $error: $errorRule;
+        $errors: $errorsRule;
+        $inline: AbolishInlineValidator;
+        $skip: boolean | ((value: any) => boolean);
+
+        default: any;
+        required: boolean;
+        typeof: string | string[] | false;
+        exact: string | boolean | number;
+        min: number;
+        max: number;
+        minLength: number;
+        maxLength: number;
+        size: number | number[];
+        object: Record<string, AbolishRule>;
+        objectAsync: Record<string, AbolishRule>;
+    }
+}
+
+/**
+ * AV - Abolish Validators
+ * alias for AvailableValidators.Options
+ */
+export type AV = Partial<AvailableValidators.Options>;
