52245f3b1ed5ad31148e9221309963fbd7326b31
trapcodeio <trapcode.io@gmail.com>
2022-07-08 09:43:56 +0100

    Add $joi and $yup types

---
+++ validators/validators.d.ts
@@ -0,0 +1,26 @@
+// Arrays
+import "./array/any";
+import "./array/array";
+import "./array/arraySize";
+import "./array/arrayValues";
+import "./array/arrayValuesAsync";
+import "./array/inArray";
+import "./array/notInArray";
+
+// Strings
+import "./string/alphaNumeric";
+import "./string/boolean";
+import "./string/date";
+import "./string/email";
+import "./string/ipAddress";
+import "./string/json";
+import "./string/jsonDecode";
+import "./string/jsonEncode";
+import "./string/md5";
+import "./string/number";
+import "./string/regex";
+import "./string/string";
+import "./string/url";
+
+// Utils
+import "./utils/same";
